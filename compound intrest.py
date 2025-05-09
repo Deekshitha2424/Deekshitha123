ade14c84096c8f53912cc331536f7600b6661121
Deekshitha2424 <deekshithagowdaar@gmail.com>
2025-05-09 09:34:44 +0530

    Add files via upload

---
+++ compound intrest.py
@@ -0,0 +1,14 @@
+def calculate_compound_intrest(princial,rate,time):
+    #convert rate from percentage to decimal
+    rate_decimal=rate/100
+    #caculate the compound intrest
+    amount=principal*(1+rate_decimal)**time
+    compound_intrest=amount-principal
+    return compound_intrest
+#input values
+principal=float(input("entre the principal amount:"))
+rate=float(input("entre the annual intrest rate(in percentage):"))
+time=float(input("entre the number of periods:"))
+#calculate the compound intrest
+intrest=calculate_compound_intrest(principal,rate,time)
+                                       
