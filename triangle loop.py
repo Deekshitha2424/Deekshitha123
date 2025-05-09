d556c80ac1d8f08be14fa9271ed6c91693b6e3c4
Deekshitha2424 <deekshithagowdaar@gmail.com>
2025-05-09 10:24:08 +0530

    Add files via upload

---
+++ triangle loop.py
@@ -0,0 +1,8 @@
+# Python program to print numbers in triangular form using for loop
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows, 0, -1):
+    for j in range(rows - i + 1):
+        print(i, end=" ")
+    print()
